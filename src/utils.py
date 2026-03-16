d87e1120a5f0a2f9917cd6fd74d2df73083e911c
Navanil <160647890+navanil-das@users.noreply.github.com>
2026-03-17 02:11:17 +0530

    Add IP formatting and banner printing functions

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,10 @@
+def format_ip(ip):
+    """Return formatted IP string"""
+    return ip.strip()
 
+
+def banner(text):
+    """Print formatted banner"""
+    print("\n" + "="*40)
+    print(text)
+    print("="*40)
